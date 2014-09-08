a60676c196529c0f13e779aa9ab95967af3d9af3
Timothy Roy <roy.tim@gmail.com>
2014-09-08 16:55:08 -0500

    -a

---
+++ Problem1.py
@@ -0,0 +1,23 @@
+"""
+Finds the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+def multiple_3_or_5 (number):
+    """
+    this implementation initalizes a counter variable and uses a simple for-loop
+    """
+    result = 0
+    for n in range(number):
+        if (n % 3==0) or (n % 5==0):
+            result += n
+    return result
+
+def second_multiple_3_or_5 (number):
+    """
+    this implementation uses a list comprehension and the built-in function sum()
+    """
+    listrange = [num for num in range(number) if num%3==0 or num%5==0]
+    return sum(listrange)
+
+print multiple_3_or_5(1000)
+print second_multiple_3_or_5(1000)
